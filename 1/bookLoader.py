3bfd731735c2509fa4b0bff2016a5a3884665f4b
lifeixiong <lifeixiong@outlook.com>
2018-03-28 21:07:19 +0800

    finish loader

--- 1/bookLoader.py
+++ 1/bookLoader.py
@@ -2,6 +2,8 @@
 
 from bs4 import BeautifulSoup
 import requests
+import os
+import io
 
 
 class BookLoader:
@@ -10,7 +12,7 @@ class BookLoader:
         self.book = book
         self.chapters = []
 
-    def getBookChapters(self):
+    def getChapters(self):
         req = requests.get(url=self.book)
         html = req.text
         bf = BeautifulSoup(html)
@@ -27,28 +29,32 @@ class BookLoader:
         title = bf.find_all('h1')[0].text.encode("utf-8")
         texts = bf.find_all('div', class_='showtxt')
         text = texts[0].text.encode("utf-8").replace('\xa0'*8, '\n\n')
-        return title + "\n\n" + text
+        return title, text
 
-    def writer(self, name, path, text):
+    def writer(self, path, title, text):
         write_flag = True
-        with open(path, 'a', encoding='utf-8') as f:
-            f.write(name + '\n')
-            f.writelines(text)
-            f.write('\n\n')
-
-    def start(self):
-        self.getBookChapters()
-        for chapter in self.chapters:
-            text = self.getChapter(chapter)
-            print(text)
-
-
+        with io.open(path, 'a', encoding='utf-8') as f:
+            f.write((title + '\n').decode("utf-8"))
+            f.writelines(text.decode("utf-8"),)
+            f.write('\n\n'.decode("utf-8"),)
 
 
 if __name__ == '__main__':
     service = 'http://www.biqukan.com'
     book = 'http://www.biqukan.com/1_1094/'
     target = 'http://www.biqukan.com/1_1094/5403177.html'
+    path = "1094.txt"
 
     bookLoader = BookLoader(service, book)
-    bookLoader.start()
+    bookLoader.getChapters()
+    length = len(bookLoader.chapters)
+    index = 0
+    print("开始下载...%s" % (length))
+    for chapter in bookLoader.chapters:
+        title, text = bookLoader.getChapter(chapter)
+        bookLoader.writer(path, title, text)
+        index += 1
+        os.system('clear')
+        print("下载%.3f%%..." % (float(index) / length * 100) + '\r')
+
+    print("下载完成！")
